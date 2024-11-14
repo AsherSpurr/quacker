42586663a63a6c9f74f6948762a57778b5185af9
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-11-14 14:17:01 -0700

    Temp change timestamp to string

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,1 +1,16 @@
+import { sql } from "@vercel/postgres";
+import { Quack } from "./definitions";
 
+export async function fetchUser(id: string) {
+
+}
+
+//Update this function to only show quacks from following list
+export async function fetchQuacks() {
+    try {
+        const data = await sql<Quack>`SELECT * FROM quacks`;
+        return data.rows;
+    } catch (error){
+        throw new Error(`Failed to fetch Quacks. ${error}`) //Update for better error handling
+    }
+}
